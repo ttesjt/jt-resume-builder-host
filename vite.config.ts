2a7b447872b645a0e5f971f7941d82a577f67bbd
Weiyan Tao <taoweiyan1998@gmail.com>
2025-02-14 07:52:36 -0800

    Make a build in docs folder for github page.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/jt-resume-builder-host/',
+  build: {
+    outDir: "docs",
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
